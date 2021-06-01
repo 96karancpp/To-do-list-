18fcf1ae4ea18217b427b296d660ae021a8612ac
Karan Kumar <96karancpp@gmail.com>
2021-06-01 23:09:38 +0530

    Inital Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <h1>To do App</h1>
+  <input type="text" v-model="todoName" @keyup.enter="addTodo">
+  <div>
+    <ul>
+      <li v-for="todo of todos" v-bind:key = "todo.id">{{todo.name}}<button>Delete</button></li>
+    </ul>
   </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+const baseURL = "http://localhost:3000/todos";
+
+Vue.use(VueAxios, axios)
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      todos:[],
+      todoName:""
+    }
+  },
+  async created(){
+    try{
+      const res = await axios.get(baseURL);
+      this.todos = res.data;
+    }catch(e){
+      console.error(e);
+    }
+  },
+  methods:
+  {
+    async addTodo()
+    {
+      const res = await axios.post(baseURL,{name:this.todoName});
+      this.todos = [...this.todos,res.data];
+      this.todoName = "";
+    }
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0px;
+  padding: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #042e58;
+  margin-top: 10px;
 }
+#app img{
+  height: 100px;
+  width: 100px;
+}
+
+
 </style>
